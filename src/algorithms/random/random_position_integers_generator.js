a17e93698e2d7deb77bc1ede3e38edd78cc826d6
sergei <red331@mail.ru>
2016-10-05 18:56:26 +0300

    Add stub for new interview question and algorithm.

---
+++ src/algorithms/random/random_position_integers_generator.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Generates array filled with unique integers in random positions
+//
+// Example
+//
+// minInteger - 5
+// maxInteger - 10
+//
+// result:
+//
+// [  ]
+
+// not best solution
+const generateRandomPositionIntegers = (minInteger, maxInteger) => {
+
+}
---
+++ src/interview_tasks/which_appears_twice.js
@@ -0,0 +1,5 @@
+'use strict';
+
+// https://www.interviewcake.com/question/javascript/which-appears-twice
+//
+// Algorithm that finds one duplicate occurrence among sequence of unique integers
