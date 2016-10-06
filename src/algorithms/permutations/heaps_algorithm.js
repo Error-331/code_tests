b2afa907c6c2f0c8d3a9a4d4b9db75a37e157bbe
sergei <red331@mail.ru>
2016-10-06 18:53:08 +0300

    Add heaps algorithm stub.

---
+++ src/algorithms/permutations/heaps_algorithm.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// https://en.wikipedia.org/wiki/Heap%27s_algorithm
+
+const heapsPermutationGenerator = (arrayToPermute) => {
+    let permutationsArray = [];
+    let arrayToPermuteLength = arrayToPermute.length;
+    let helperArray = Array(arrayToPermuteLength).fill(0);
+
+    console.log(helperArray);
+}
+
+console.log(heapsPermutationGenerator([1,2,3,4]));
