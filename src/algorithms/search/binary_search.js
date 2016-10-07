cc5a3b15d4680b5bbac6300c7019b903e2cbb4de
sergei <red331@mail.ru>
2016-10-07 19:20:03 +0300

    Add stub for binary search algorithm.

---
+++ src/algorithms/search/binary_search.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// https://www.tutorialspoint.com/data_structures_algorithms/binary_search_algorithm.htm
+// https://en.wikipedia.org/wiki/Binary_search_algorithm
+
+// mock data
+let sortedIntsArray1 = [-1, 2, 4, 5, 7];
+let sortedIntsArray2 = [-9 , -3, -2, 1, 8, 10];
+
+// good solution
+const binarySearchOfIntegerGoodSolution = (sortedInts, intToFind) => {
+    let sortedIntsSize = sortedInts.length;
+
+    let lowerBound = 0;
+    let upperBound = sortedIntsSize - 1;
+
+
+}
