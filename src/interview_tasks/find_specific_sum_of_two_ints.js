b0808dca0f995435f91b85e22030f536061759a0
sergei <red331@mail.ru>
2016-10-11 16:13:04 +0300

    Add binary  search solution.

--- src/interview_tasks/find_specific_sum_of_two_ints.js
+++ src/interview_tasks/find_specific_sum_of_two_ints.js
@@ -6,6 +6,39 @@
 let intsArray1 = [4, 7, 2, -1, 5];
 let intsArray2 = [-2, 8, -3, -9, 10, 1];
 
+// binary search algorithm
+const binarySearchOfInteger = (sortedInts, intToFind) => {
+    let sortedIntsSize = sortedInts.length;
+    let searchValuePosition = -1;
+
+    let lowerBound = 0;
+    let upperBound = sortedIntsSize - 1;
+    let midPoint;
+
+    while (searchValuePosition === -1) {
+        if (upperBound < lowerBound) {
+            return -1;
+        }
+
+        midPoint = lowerBound + parseInt(( upperBound - lowerBound ) / 2);
+
+        if (sortedInts[midPoint] === intToFind) {
+            searchValuePosition = midPoint;
+        }
+
+        if (sortedInts[midPoint] < intToFind) {
+            lowerBound = midPoint + 1;
+        }
+
+        if (sortedInts[midPoint] > intToFind) {
+            upperBound = midPoint - 1;
+        }
+
+    }
+
+    return searchValuePosition;
+}
+
 // best solution
 const findSpecificSumOfTwoIntsBestSolution = (intsArray, specificSum) => {
     let intsArrayCopy = intsArray.slice().sort((firstInt, secondInt) => {
@@ -32,6 +65,27 @@ const findSpecificSumOfTwoIntsBestSolution = (intsArray, specificSum) => {
     return false;
 }
 
+// binary search solution
+const findSpecificSumOfTwoIntsBinarySearchSolution = (intsArray, specificSum) => {
+    let intsArrayCopy = intsArray.slice().sort((firstInt, secondInt) => {
+        return firstInt - secondInt;
+    });
+
+    let intsArrayLength = intsArrayCopy.length;
+
+    for (let intIndex = 0; intIndex < intsArrayLength; intIndex++) {
+        let firstInt = intsArrayCopy[intIndex];
+        let secondInt = specificSum - firstInt;
+        let searchIndex = binarySearchOfInteger(intsArrayCopy, secondInt);
+
+        if (searchIndex !== -1 && searchIndex !== intIndex) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 console.log('Sum of two elements equality to use sum');
 console.log('=======================================');
 
@@ -48,3 +102,17 @@ console.log('');
 console.log('Array: 2, sum: -11, isFound: ' + findSpecificSumOfTwoIntsBestSolution(intsArray2, -11)); // true
 console.log('Array: 2, sum: -3, isFound: ' + findSpecificSumOfTwoIntsBestSolution(intsArray2, -3)); // false
 console.log('Array: 2, sum: -5, isFound: ' + findSpecificSumOfTwoIntsBestSolution(intsArray2, -5)); // true
+
+console.log('');
+console.log('Binary search solution');
+console.log('');
+
+console.log('Array: 1, sum: 6, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray1, 6)); // true
+console.log('Array: 1, sum: 9, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray1, 9)); // true
+console.log('Array: 1, sum: 10, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray1, 10)); // false
+
+console.log('');
+
+console.log('Array: 2, sum: -11, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray2, -11)); // true
+console.log('Array: 2, sum: -3, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray2, -3)); // false
+console.log('Array: 2, sum: -5, isFound: ' + findSpecificSumOfTwoIntsBinarySearchSolution(intsArray2, -5)); // true
