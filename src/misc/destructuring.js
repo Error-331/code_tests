d5f6302fcf8ec0f3de86ac11240f19e638093981
sergei <red331@mail.ru>
2016-10-28 16:03:07 +0300

    Add destructuring operators examples.

--- src/misc/destructuring.js
+++ src/misc/destructuring.js
@@ -1,5 +1,26 @@
-export const destructuringMisc1 = () => {
-    let a = 5;
+'use strict';
 
-    console.log(a);
+// destructuring example 1
+let [first1, ...rest1] = [1, 2, 3, 4];
+console.log(first1); // returns 1
+console.log(rest1); // returns [ 2, 3, 4 ]
+
+// destructuring example 2
+let [first2] = [1, 2, 3, 4];
+console.log(first2); // outputs 1
+
+// destructuring example 3
+let [, second3, , fourth3] = [1, 2, 3, 4];
+console.log(second3); // returns 2
+console.log(fourth3); // returns 4
+
+// destructuring example 4
+let testObj1 = {
+    to1prop1: 'foo',
+    to1prop2: 12,
+    to1prop3: 'bar'
 }
+
+let {to1prop2, to1prop3} = testObj1;
+console.log(to1prop2); // returns 12
+console.log(to1prop3); // return 'bar'
