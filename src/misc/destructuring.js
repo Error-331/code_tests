89081daa3d9f15288d7a76acd741f6fc0a5d9ec5
sergei <red331@mail.ru>
2016-11-30 19:40:41 +0200

    Extend destructuring examples.

--- src/misc/destructuring.js
+++ src/misc/destructuring.js
@@ -24,3 +24,21 @@ let testObj1 = {
 let {to1prop2, to1prop3} = testObj1;
 console.log(to1prop2); // returns 12
 console.log(to1prop3); // return 'bar'
+
+// destructuring example 4
+let testObj2 = {
+    to2prop1: 'foo1',
+    to2prop2: 12,
+    to2prop3: 'bar1',
+    to2Prop4: {
+        to2SubProp1: 'foo2',
+        to2SubProp2: 13,
+        to2SubProp3: 'bar2'
+    }
+};
+
+const {to2prop1, to2prop3, to2Prop4: {to2SubProp2}} = testObj2;
+
+console.log(to2prop1);
+console.log(to2prop3);
+console.log(to2SubProp2);
