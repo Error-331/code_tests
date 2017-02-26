eb9f2cacfb90413b11d1e73631b6d498ce958031
Sergei Selihov <red331@mail.ru>
2017-02-26 20:29:09 +0200

    ...includes.

--- src/vanilla_js/index.js
+++ src/vanilla_js/index.js
@@ -1,15 +1,25 @@
 'use strict';
 
+import symbols from './fundamentals/symbols';
+
 import combinators from './functional/combinators';
 import compose from './functional/compose';
 import basics from './functional/basics';
+import transducers from './functional/transducers';
+import promises from './functional/promises';
+import lenses from './functional/lenses';
 
 async function run() {
+    // fundamentals
+    await symbols();
 
     // functional programming
     await combinators();
     await compose();
     await basics();
+    await transducers();
+    await promises();
+    await lenses();
 }
 
 run();
