a10bd6783001dd9c8d5f1122310ff31ff7f1de1f
Sergei Selihov <red331@mail.ru>
2017-05-13 22:21:17 +0300

    Extend 'partial application' functional example.

--- src/vanilla_js/functional/fundamentals/partial_application.js
+++ src/vanilla_js/functional/fundamentals/partial_application.js
@@ -18,8 +18,18 @@ export default async () => {
         };
     };
 
+    Function.prototype.partialApplyRight = function(...superArguments){
+        const self = this;
+
+        return function(...subArguments){
+            return self.call(this, [...subArguments][0], ...superArguments);
+        };
+    };
+
+
     const testOUI = 123;
     const getMacAddress = numbersToHex.partialApplyLeft(testOUI);
+    const shadesOfBlue = numbersToHex.partialApplyRight(255);
 
     console.log('Partial application');
     console.log('===================');
@@ -34,11 +44,19 @@ export default async () => {
 
     console.log('');
 
-    console.log('Partly applied left function(numbersToHex):');
+    console.log('Partly applied left numbersToHex():');
     console.log('');
 
     console.log('getMacAddress() -', getMacAddress()); // '7b'
-    console.log('getMacAddress(100, 200, 2, 123, 66, 0, 1) -', getMacAddress(100, 200, 2, 123, 66, 0, 1)); // '7b64c8027b420001'
+    console.log('getMacAddress(100, 200, 2, 123, 66, 0, 1) -', getMacAddress(100, 200, 2, 123, 66, 0, 1));
+
+    console.log('');
+
+    console.log('Partly applied right numbersToHex():');
+    console.log('');
+
+    console.log('shadesOfBlue(123, 0) -', shadesOfBlue(123, 0));
+    console.log('shadesOfBlue(100, 200) -', shadesOfBlue(100, 200));
 
     console.log('');
     console.log('--------------------------------------------------------');
