904baa575a23af535b92c8808711ce8b294c36c6
Sergei Selihov <red331@mail.ru>
2017-05-15 00:17:37 +0300

    Add fundamental/numbers examples.

---
+++ src/vanilla_js/fundamentals/numbers.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// http://0.30000000000000004.com/
+
+export default async () => {
+    console.log('Numbers');
+    console.log('=======');
+    console.log('');
+
+    console.log('Floating point math quirks:');
+    console.log('');
+
+    console.log('0.1 + 0.2 =', 0.1 + 0.2);
+    console.log('.1 + .2 =', .1 + .2);
+
+    console.log('');
+    console.log('--------------------------------------------------------');
+    console.log('');
+}
