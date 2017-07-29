68bd43395db18bfa0480ea94c9dd182258e2a74d
Sergei Selihov <red331@mail.ru>
2017-07-30 02:05:54 +0300

    More ts decorators examples.

--- src/type_script/decorators.ts
+++ src/type_script/decorators.ts
@@ -1,14 +1,5 @@
 'use strict';
 
-
-
-
-
-// method testing
-//console.log(testObj.testMethod1()); // returns 'test_val_1_static' instead of 'test_val_1'
-//console.log(testObj.testMethod2(2)); // returns 9
-//https://www.typescriptlang.org/docs/handbook/decorators.html#class-decorators
-
 export default () => {
     // decorators definition
     function classDecorator1<T extends {new(...args:any[]):{}}>(constructor:T) {
@@ -21,60 +12,78 @@ export default () => {
     function classDecorator2(constructor: Function) {
         Object.seal(constructor);
         Object.seal(constructor.prototype);
-
-        console.log(Object.isSealed(constructor));
-        console.log(Object.isSealed(constructor.prototype));
     }
 
-    function classDecorator3(constructor: Function) {
-        console.log(Object.isSealed(constructor));
-        console.log(Object.isSealed(constructor.prototype));
+    function MethodDecorator1(prototype, propertyKey, descriptor) {
+        let result = prototype[propertyKey]();
+        descriptor.value = () => {return result + '_static'};
+        return descriptor;
+    }
+
+    function MethodDecorator2(prototype, propertyKey, descriptor) {
+        let originalFunc = descriptor.value;
+
+        descriptor.value = function(intToMult) {
+            return originalFunc.call(this, intToMult) + 5;
+        };
+
+        return descriptor;
+    }
+
+    function MethodDecorator3(suffix: string) {
+        return function(prototype, propertyKey, descriptor) {
+            let result = prototype[propertyKey]();
+            descriptor.value = () => {return result + suffix};
+            return descriptor;
+        }
+    }
+
+    function MethodDecorator4(newValue: string) {
+        return function(prototype, propertyKey, descriptor) {
+            descriptor.get = function() {
+                return newValue;
+            }
+        }
     }
 
     // class mock definition
     @classDecorator1
     @classDecorator2
-    @classDecorator3
     class testClass1 {
         public testProp1: string;
         public testProp2: string;
         public testProp3: string;
 
+        protected _testProp4: string;
+
         constructor() {
             this.testProp1 = 'test_prop1_val1';
             this.testProp2 = 'test_prop2_val1';
             this.testProp3 = 'test_prop3_val1';
+
+            this._testProp4 = 'test_prop4_val1'
         }
 
-       // @TestDecorator1
+        @MethodDecorator1
         testMethod1() {
             return 'test_val_1'
         }
 
-        //@TestDecorator2
+        @MethodDecorator2
         testMethod2(intToMult) {
             return intToMult * 2;
         }
+
+        @MethodDecorator3('_decorated')
+        testMethod3() {
+            return 'test_val_3';
+        }
+
+        @MethodDecorator4('new_test_prop4_val1')
+        get testProp4() {
+            return this._testProp4;
+        }
     }
-
-
-    /*function TestDecorator1(target, propertyKey, descriptor) {
-        let result = target[propertyKey]();
-        descriptor.value = () => {return result + '_static'}
-        return descriptor;
-    }
-
-    function TestDecorator2(target, propertyKey, descriptor) {
-        let originalFunc = descriptor.value;
-
-        descriptor.value = function(intToMult) {
-
-            console.log(originalFunc);
-            return originalFunc.call(this, intToMult) + 5;
-        };
-
-        return descriptor;
-    }*/
 
     console.log('TypeScript decorators examples');
     console.log('==============================');
@@ -82,11 +91,19 @@ export default () => {
 
     const testObj1 = new testClass1();
 
-    console.log(testObj1.testProp1);
-    console.log(testObj1.testProp2);
-    console.log(testObj1.testProp3);
+    console.log('testObj1.testProp1 -', testObj1.testProp1);
+    console.log('testObj1.testProp2 -', testObj1.testProp2);
+    console.log('testObj1.testProp3 -', testObj1.testProp3);
 
-    console.log(Object.isSealed(testClass1.constructor));
+    console.log('');
+
+    console.log('testObj1.testMethod1() -', testObj1.testMethod1());
+    console.log('testObj1.testMethod2(5) -', testObj1.testMethod2(5));
+    console.log('testObj1.testMethod3() -', testObj1.testMethod3());
+
+    console.log('');
+
+    console.log('testObj1.testProp4 -', testObj1.testProp4);
 
     console.log('');
     console.log('--------------------------------------------------------');
