ca206af4a7833e4023ef993a81a79b1908cc7058
ERROR-331 <red331@mail.ru>
2017-07-31 20:51:48 +0300

    Add more basic types.

--- src/type_script/basic_types.ts
+++ src/type_script/basic_types.ts
@@ -49,6 +49,16 @@ export default async () => {
     console.log('let anyVar3: any = "str1";');
 
     console.log('');
+    console.log('null/undefined vars declaration:');
+    console.log('');
+
+    let undefinedVar1: undefined = undefined;
+    let nullVar1: null = null;
+
+    console.log('let undefinedVar1: undefined = undefined;');
+    console.log('let nullVar1: null = null;');
+
+    console.log('');
     console.log('Array vars declaration:');
     console.log('');
 
@@ -132,6 +142,16 @@ export default async () => {
 
     console.log('function voidFunc1(): void {console.log("void func 1");}');
     console.log('function voidFunc2(): void {console.log("void func 2");}');
+
+    console.log('');
+    console.log('Declaration of function that never returns value:');
+    console.log('');
+
+    function neverFunc1(message: string): never {
+        throw new Error(message);
+    }
+
+    console.log('function neverFunc1(message: string): never {throw new Error(message);}');
 
     console.log('');
     console.log('--------------------------------------------------------');
