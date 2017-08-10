4a066e0f5754a0a82381d602fd7818ea09064ad2
Sergei Selihov <red331@mail.ru>
2017-08-10 16:58:33 +0300

    Add string enum examples, update libraries.

--- src/type_script/index.ts
+++ src/type_script/index.ts
@@ -6,18 +6,18 @@ import basicTypes from './basic_types';
 //import classes from './classes';
 //import functions from './functions';
 //import generics from './generics';
-//import enums from './enums';
+import enums from './enums';
 //import rest_spread from './rest_spread';
 //import async_await from './async_await';
 //import decorators from './decorators';
 
 async function run() {
-    await basicTypes();
+    //await basicTypes();
     //await interfaces();
     //await classes();
     //await functions();
     //await generics();
-    //await enums();
+    await enums();
     //await rest_spread();
     //await async_await();
     //await decorators();
