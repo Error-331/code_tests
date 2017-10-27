7b37df2ebf092c3e8ebfb437a0cbf23ec58f0953
Sergei Selihov <red331@mail.ru>
2017-10-27 19:40:48 +0300

    Add additional methods, code refacktoring.

--- src/node_js/server/js/mixins/static_server_mixin.js
+++ src/node_js/server/js/mixins/static_server_mixin.js
@@ -3,66 +3,92 @@
 const fs = require('fs');
 
 const {
+    parseURLPathParams,
     extractFileExtensionFromPathParams,
     extractFileNameFromPathParams,
     getMIMETypeForFileExtension
 } = require('./../utils/server_request_utils');
 
 const StaticServerMixin = (superClass) => class extends superClass {
-    _serveStaticFileByURLParams() {
-        return new Promise(async (resolve, reject) => {
-            let pathToFile;
+    _preparePathToFile(pathParams) {
+        pathParams = pathParams.slice();
 
-            const fileExtension = extractFileExtensionFromPathParams(this._urlPathParams);
-            const fileName = extractFileNameFromPathParams(this._urlPathParams);
+        const fileName = extractFileNameFromPathParams(pathParams);
+        const fileExtension = extractFileExtensionFromPathParams(pathParams);
 
-            const fileMIMEType = getMIMETypeForFileExtension(fileExtension);
+        pathParams.pop();
 
-            if (!fileMIMEType) {
-                const errorMessage = `Cannot find MIME type for file extension of ".${fileExtension}"`;
+        const pathParamsCopyNormalized = pathParams.map(pathParam => pathParam.toLocaleLowerCase());
+        const pathToDirectory = pathParamsCopyNormalized.length > 0 ? `/${pathParamsCopyNormalized.join('/')}/` : '/';
 
-                this._serveErrorPage(400, errorMessage);
-                return reject(new Error(errorMessage));
-            }
+        let pathToFile;
 
-            const pathParamsCopy = this._urlPathParams.slice();
-            pathParamsCopy.pop();
+        if (fileExtension === 'html') {
+            pathToFile = `${this._serverRootDir}/${this._getHTMLPagesDirectoryPath()}${pathToDirectory}${fileName}.${fileExtension}`;
+        } else {
+            pathToFile = `${this._serverRootDir}/${this._getResourcesDirectoryPath()}${pathToDirectory}${fileName}.${fileExtension}`;
+        }
 
-            const pathParamsCopyNormalized = pathParamsCopy.map(pathParam => pathParam.toLocaleLowerCase());
-            const pathToDirectory = pathParamsCopyNormalized.length > 0 ? `/${pathParamsCopyNormalized.join('/')}/` : '/';
+        return pathToFile;
+    }
 
-            if (fileExtension === 'html') {
-                pathToFile = `${this._serverRootDir}/${this._getHTMLPagesDirectoryPath()}${pathToDirectory}${fileName}.${fileExtension}`;
-            } else {
-                pathToFile = `${this._serverRootDir}/${this._getResourcesDirectoryPath()}${pathToDirectory}${fileName}.${fileExtension}`;
-            }
+    _serveFile(pathToFile, fileMIMEType) {
+        return new Promise((resolve, reject) => {
+            this._addResponseHeader('Content-Type', fileMIMEType);
+            this._writeHead(200);
 
             const staticFileStream = fs.createReadStream(pathToFile, {
                 flags: 'r',
                 autoClose: true
             });
 
-            this._addResponseHeader('Content-Type', fileMIMEType);
-            this._writeHead(200);
-
             staticFileStream.on('close', () => {
                 resolve();
             });
 
             staticFileStream.on('error', (error) => {
-                console.log(error);
+                const isENOENT = error.code === 'ENOENT';
 
-                if (error.code === 'ENOENT') {
-                    this._serveErrorPage(404, `Cannot find file: "${fileName}.${fileExtension}"`);
-                } else {
-                    this._serveErrorPage(400, `Cannot open file: "${fileName}.${fileExtension}"`);
-                }
+                const pathParams = pathToFile.split('/');
+                const fileName = pathParams[pathParams.length - 1];
 
-                resolve();
+                const errorCode = isENOENT ? 404 : 400;
+                const errorMessage = isENOENT ? `Cannot find file: "${fileName}"` : `Cannot open file: "${fileName}.${fileExtension}"`;
+
+                this._serveErrorPage(errorCode, errorMessage);
+                reject(new Error(errorMessage));
             });
 
             staticFileStream.pipe(this._response);
         });
+    }
+
+    async _serverStaticFileByPath(pathParams) {
+        pathParams = typeof pathParams === 'string' ? parseURLPathParams(pathParams) : pathParams.slice();
+
+        if (!pathParams || pathParams.length <= 0) {
+            const errorMessage = `Cannot serve undefined file`;
+
+            this._serveErrorPage(400, errorMessage);
+            throw new Error(errorMessage);
+        }
+
+        const fileExtension = extractFileExtensionFromPathParams(pathParams);
+        const fileMIMEType = getMIMETypeForFileExtension(fileExtension);
+
+        if (!fileMIMEType) {
+            const errorMessage = `Cannot find MIME type for file extension of ".${fileExtension}"`;
+
+            this._serveErrorPage(400, errorMessage);
+            throw new Error(errorMessage);
+        }
+
+        const pathToFile = this._preparePathToFile(pathParams);
+        await this._serveFile(pathToFile, fileMIMEType);
+    }
+
+    _serveStaticFileByURLParams() {
+        return this._serverStaticFileByPath(this._urlPathParams);
     };
 
     constructor(...serverParams) {
