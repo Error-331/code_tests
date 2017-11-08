c8e7fd14b6f69d98f4996c87a715037a72989733
Sergei Selihov <red331@mail.ru>
2017-11-09 01:14:49 +0200

    Start working on web socket server.

---
+++ src/node_js/server/js/classes/basic_web_socket_server_class.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const {parseHTTPRequest} = require('./../utils/server_request_utils');
+const {generateSHA1, encodeStringToBase64} = require('./../utils/crypto_utils');
+
+class BasicWebSocketServerClass {
+
+    _onWebSocketConnect() {
+        this._isDebugEnabled && console.log('WebSocket event: connect');
+    }
+
+    _onWebSocketClose() {
+        this._isDebugEnabled && console.log('WebSocket event: close');
+    }
+
+    _onWebSocketData(data) {
+        this._isDebugEnabled && console.log('WebSocket event: data, message: ', data.toString());
+
+        // binary = latin1
+        const stringData = data.toString('binary');
+
+        if (!this._isWebSocketConnected) {
+            this._performHandshake(stringData);
+        } else {
+
+        }
+
+
+
+       /*
+
+
+
+        if (this._isWebSocketConnected) {
+            // do something
+        }
+        else {
+            this._performHandshake(stringData);
+
+           /* var response = HandshakeHYBI00(data.toString('binary'));
+            if (response) {
+                // handshake succeeded, open connection
+                socket.write(response.join('\r\n'), 'binary');
+                wsConnected = true;
+            }
+            else {
+                // close connection, handshake bad
+                socket.end();
+                return;
+            }*/
+       // }
+    }
+
+    _onWebSocketDrain() {
+        this._isDebugEnabled && console.log('WebSocket event: drain');
+    }
+
+    _onWebSocketEnd() {
+        this._isDebugEnabled && console.log('WebSocket event: end');
+
+     //   const concatedData = concatBuffers(this._dataChunks);
+     //   this._dataChunks = [];
+
+     //   console.log(concatedData);
+    }
+
+    _onWebSocketLookup() {
+        this._isDebugEnabled && console.log('WebSocket event: lookup');
+    }
+
+    _onWebSocketTimeout() {
+        this._isDebugEnabled && console.log('WebSocket event: timeout');
+    }
+
+    _onWebSocketError(error) {
+        console.error(error);
+    }
+
+    _bindEventHandlers() {
+        this._socket.on('connect', this._onWebSocketConnect.bind(this));
+        this._socket.on('close', this._onWebSocketClose.bind(this));
+        this._socket.on('data', this._onWebSocketData.bind(this));
+        this._socket.on('drain', this._onWebSocketDrain.bind(this));
+        this._socket.on('end', this._onWebSocketEnd.bind(this));
+        this._socket.on('lookup', this._onWebSocketLookup.bind(this));
+        this._socket.on('timeout', this._onWebSocketTimeout.bind(this));
+        this._socket.on('error', this._onWebSocketError.bind(this));
+    }
+
+    // TODO: Sec-WebSocket-Protocol
+    _performHandshake(receivedData) {
+        const httpRequestData = parseHTTPRequest(receivedData);
+
+        if (!this._isWebSocketOpenRequest(httpRequestData)) {
+            // do something
+            return;
+        }
+
+        // save necessary data sent from client during handshake
+        this._secWebSocketKeyClient = httpRequestData.headers['sec-websocket-key'];
+        this._secWebSocketVersion = parseInt(httpRequestData.headers['sec-websocket-version']);
+        this._secWebSocketExtension = httpRequestData.headers['sec-websocket-extensions'];
+
+        const guid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
+        const responseKey = `${this._secWebSocketKeyClient}${guid}`;
+
+        const responseKeyBase64 = generateSHA1(responseKey, 'base64');
+
+        this._socket.write('HTTP/1.1 101 Switching Protocols\r\n', 'binary');
+        this._socket.write(`Sec-WebSocket-Accept:${responseKeyBase64}\r\n`, 'binary');
+        this._socket.write('Upgrade:websocket\r\n', 'binary');
+        this._socket.write('Connection:Upgrade\r\n', 'binary');
+        this._socket.write('\r\n');
+    }
+
+    _isWebSocketOpenRequest(httpRequestData) {
+        const secWebSocketKeyHeader = httpRequestData.headers['sec-websocket-key'];
+        const secWebSocketVersionHeader = httpRequestData.headers['sec-websocket-version'];
+        const secWebSocketExtensionsHeader = httpRequestData.headers['sec-websocket-extensions'];
+        const upgradeHeader = httpRequestData.headers['upgrade'];
+
+        return secWebSocketKeyHeader && secWebSocketVersionHeader && secWebSocketExtensionsHeader && upgradeHeader;
+    }
+
+    constructor(socket, options) {
+        this._socket = socket;
+        this._isWebSocketConnected = false;
+
+        this._isDebugEnabled = !!options.enableDebug;
+
+        this._secWebSocketKeyClient = null;
+        this._secWebSocketVersion = null;
+        this._secWebSocketExtension = null;
+
+        this._bindEventHandlers();
+    }
+}
+
+module.exports = BasicWebSocketServerClass;
--- src/node_js/server/server.js
+++ src/node_js/server/server.js
@@ -10,13 +10,15 @@ const https = require('https');
 const net = require('net');
 
 const BasicServerClass = require('./js/classes/basic_server_class');
+const BasicWebSocketServerClass = require('./js/classes/basic_web_socket_server_class');
+
 const CookiesServerMixin = require('./js/mixins/cookies_server_mixin');
 const StaticServerMixin = require('./js/mixins/static_server_mixin');
 const JSONServerMixin = require('./js/mixins/json_server_mixin');
 const ETagTrackingServerMixin = require('./js/mixins/etag_tracking_server_mixin');
 const OpenProxyServerMixin = require('./js/mixins/open_proxy_server_mixin');
 
-const {HTTP_SERVER_PORT, HTTPS_SERVER_PORT} = require ('./js/constants/general_server_constants');
+const {HTTP_SERVER_PORT, HTTPS_SERVER_PORT, WEB_SOCKET_SERVER_POST, WEB_SOCKET_HOST} = require ('./js/constants/general_server_constants');
 const routes = require('./js/routes');
 
 class MainServerClass extends OpenProxyServerMixin(ETagTrackingServerMixin(JSONServerMixin((CookiesServerMixin(StaticServerMixin(BasicServerClass)))))) {
@@ -27,57 +29,20 @@ const httpWebServerRequestHandler = async (request, response) => {
     await serverClassInstance.onHandleRequest();
 };
 
-const httpsWebServerRequestHandler = async (request, response) => {
-    const serverClassInstance = new MainServerClass(request, response, {isHTTPS: true}, routes, __dirname);
-    await serverClassInstance.onHandleRequest();
-};
-
-const socketServerRequestHandler = (socket) => {
-
-    const isWebSocketConnected = false;
-
-    socket.addListener('connect', () => {
-        console.log('Web socket - connect');
-    });
-
-    socket.addListener('close', () => {
-        console.log('Web socket - close');
-    });
-
-    socket.addListener('data', (data) => {
-        console.log('Web socket - data');
-    });
-
-    socket.addListener('drain', () => {
-        console.log('Web socket - drain');
-    });
-
-    socket.addListener('end', () => {
-        console.log('Web socket - end');
-    });
-
-    socket.addListener('lookup', () => {
-        console.log('Web socket - lookup');
-    });
-
-    socket.addListener('timeout', () => {
-        console.log('Web socket - timeout');
-    });
-
-    socket.addListener('error', (error) => {
-        console.error('Web socket - error:', error);
-    });
-};
-
 const httpWebServer = http.createServer(httpWebServerRequestHandler);
 
 httpWebServer.listen(HTTP_SERVER_PORT, (error) => {
     if (error) {
-        return console.error('Error while starting server (http) -', error);
+        console.error('Error while starting server (http) -', error);
     } else {
         console.log(`Server (http) is listening on ${HTTP_SERVER_PORT}`);
     }
 });
+
+const httpsWebServerRequestHandler = async (request, response) => {
+    const serverClassInstance = new MainServerClass(request, response, {isHTTPS: true}, routes, __dirname);
+    await serverClassInstance.onHandleRequest();
+};
 
 const httpsWebServer = https.createServer({
     key: readFileSync('./ssl/server.key', 'utf8'),
@@ -87,12 +52,19 @@ const httpsWebServer = https.createServer({
 
 httpsWebServer.listen(HTTPS_SERVER_PORT, (error) => {
     if (error) {
-        return console.error('Error while starting server (https) -', error);
+        console.error('Error while starting server (https) -', error);
     } else {
         console.log(`Server (https) is listening on ${HTTPS_SERVER_PORT}`);
     }
 });
 
+const socketServerRequestHandler = (socket) => {
+    console.log('Socket handler...');
+    const webSocketServerClassInstance = new BasicWebSocketServerClass(socket, {enableDebug: true});
+};
+
 const webSocketServer = net.createServer(socketServerRequestHandler);
 
-webSocketServer.listen({port: 8080, host: 'localhost'} , () => console.log('Web socket server is bound'));
+webSocketServer.listen({port:  WEB_SOCKET_SERVER_POST, host: WEB_SOCKET_HOST} , () => {
+    console.log(`Server (web-socket) is listening on ${WEB_SOCKET_SERVER_POST} at ${WEB_SOCKET_HOST}`);
+});
