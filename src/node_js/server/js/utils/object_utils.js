f505a59abbd0980a377fb29dbe254da287051c31
Sergei Selihov <red331@mail.ru>
2017-12-02 17:51:20 +0200

    Add Object utils.

---
+++ src/node_js/server/js/utils/object_utils.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const isRegularObject = objectToTest => typeof objectToTest === 'object' && objectToTest !== null;
+
+const cloneDeep = (objectToClone) => {
+    const newObject = Array.isArray(objectToClone) ? objectToClone.slice() : Object.assign({}, objectToClone);
+
+    for (const objectProperty in newObject) {
+        if (newObject.hasOwnProperty(objectProperty)) {
+            const propertyValue = newObject[objectProperty];
+
+            if (Array.isArray(propertyValue) || isRegularObject(propertyValue)) {
+                newObject[objectProperty] = cloneDeep(propertyValue);
+            }
+        }
+    }
+
+    return newObject;
+};
+
+module.exports.cloneDeep = cloneDeep;
