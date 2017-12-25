8f9455247152f6ef2d0cb2140e73e0ad5931e94d
Sergei Selihov <red331@mail.ru>
2017-12-25 03:05:47 +0200

    Beging working on chess example, minor fixes.

---
+++ src/playground/tap_media/chess/index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+// https://chessprogramming.wikispaces.com/10x12+Board
+// https://chessprogramming.wikispaces.com/Move+Generation
+
+const RANKS = 8; // rows (1-8)
+const FILES = 8; // columns
+
+const FILE_CHARACTERS = "abcdefgh";
+const RANK_CHARACTERS = "12345678";
+
+const PIECE_NAME_TO_INDEX = {
+    PAWN: 0,
+    KNIGHT: 1,
+    BISHOP: 2,
+    ROOK: 3,
+    QUEEN: 4,
+    KING: 5
+}; // seal and freez
+
+const PIECE_COLOR_INDEX = {
+    WHITE: 0,
+    BLACK: 1
+};// seal and freez
+
+const PIECE_NAMES =  [ "pawn", "knight", "bishop", "rook", "queen", "king" ]; // seal and freez
+
+const mailbox120 = [
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7, -1,
+    -1,  8,  9, 10, 11, 12, 13, 14, 15, -1,
+    -1, 16, 17, 18, 19, 20, 21, 22, 23, -1,
+    -1, 24, 25, 26, 27, 28, 29, 30, 31, -1,
+    -1, 32, 33, 34, 35, 36, 37, 38, 39, -1,
+    -1, 40, 41, 42, 43, 44, 45, 46, 47, -1,
+    -1, 48, 49, 50, 51, 52, 53, 54, 55, -1,
+    -1, 56, 57, 58, 59, 60, 61, 62, 63, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1
+];
+
+const mailbox64 = [
+    21, 22, 23, 24, 25, 26, 27, 28,
+    31, 32, 33, 34, 35, 36, 37, 38,
+    41, 42, 43, 44, 45, 46, 47, 48,
+    51, 52, 53, 54, 55, 56, 57, 58,
+    61, 62, 63, 64, 65, 66, 67, 68,
+    71, 72, 73, 74, 75, 76, 77, 78,
+    81, 82, 83, 84, 85, 86, 87, 88,
+    91, 92, 93, 94, 95, 96, 97, 98
+];
+
+const pieceData = {
+    pieceTypeIndex: 'BISHOP',
+    piecePositionMailbox64: 42
+};
+
+
+
+const calculateBishopPiecePossibleMoves = (pieceData) => {
+
+};
+
+function calculateMoveSuggestions() {
+
+}
