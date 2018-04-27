c27d35ff5e156d2da796438d552478fe21c69899
Sergei Selihov <red331@mail.ru>
2018-04-27 23:40:14 +0300

    Add opcodes.

--- src/node_js/server/js/constants/web_socket_server_general_constants.js
+++ src/node_js/server/js/constants/web_socket_server_general_constants.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const MESSAGE_TYPE_TO_OPCODE = Object.freeze({
-    'TEXT':   1,
-    'BINARY': 2,
-    'CLOSE':  8,
-    'PING':   9,
-    'PONG':   10
+    'TEXT':   1, // 0001
+    'BINARY': 2, // 0010
+    'CLOSE':  8, // 1000
+    'PING':   9, // 1001
+    'PONG':   10 // 1010
 });
 
 module.exports.MESSAGE_TYPE_TO_OPCODE = MESSAGE_TYPE_TO_OPCODE;
