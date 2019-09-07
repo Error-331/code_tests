1204f92537ac7d2c1d959f106a06e398142cf93f
Error-331 <red331@mail.ru>
2019-09-08 00:46:21 +0300

    Add master process tasks, work in progress.

---
+++ src/vendor_library_tests/functional/lodash/effects/master_process_effects.js
@@ -0,0 +1,101 @@
+'use strict';
+
+// external imports
+const {stubTrue, equals, cond} = require('lodash/fp');
+
+const chalk = require('chalk');
+const {bind} = require('lodash/fp');
+
+// local imports
+const {
+    INSERT_NEW_MODULE_NAME_CHILD_PROCESS,
+    SELECT_MODULE_CHILD_PROCESS_CHILD_PROCESS,
+    SELECT_INSERT_MODULE_NAME_CHILD_PROCESS,
+
+    INSERT_NEW_MODULE_VERSION_CHILD_PROCESS,
+    SELECT_MODULE_BY_NAME_ID_AND_VERSION_CHILD_PROCESS,
+    SELECT_INSERT_MODULE_VERSION_CHILD_PROCESS,
+
+    INSERT_NEW_MODULE_LOCATION_CHILD_PROCESS,
+    SELECT_LOCATION_BY_PATH_CHILD_PROCESS,
+    SELECT_LOCATION_BY_NAME_ID_AND_VERSION_CHILD_PROCESS,
+    SELECT_INSERT_MODULE_LOCATION_CHILD_PROCESS,
+
+    INSERT_NEW_MODULE_LOCATION_CONNECTION_CHILD_PROCESS,
+
+    INSERT_NEW_PATH_CHILD_PROCESS,
+    SELECT_TRAVERSED_PATH_BY_PATH_CHILD_PROCESS,
+} = require('./../constants/child_process_db_constants');
+
+const {
+    getModulesNamesQueryWrappers,
+    getModulesVersionsQueryWrappers,
+    getModulesLocationsQueryWrappers,
+    getModulesLocationConnectionsQueryWrappers,
+    getPathsTraversedQueryWrappers,
+} = require('./../helpers/db_helpers');
+
+// effects implementation
+
+// returns promise
+const executeChildProcessTask = (dbType, dbConnection, taskType, data) => cond([
+    [
+        equals(INSERT_NEW_MODULE_NAME_CHILD_PROCESS),
+        () => getModulesNamesQueryWrappers(dbType).insertNewModuleName(data.name, data.belongsToOrganization),
+    ],
+    [
+        equals(SELECT_MODULE_CHILD_PROCESS_CHILD_PROCESS),
+        () => getModulesNamesQueryWrappers(dbType).selectModule(data.usrName, data.belongsToOrganization),
+    ],
+    [
+        equals(SELECT_INSERT_MODULE_NAME_CHILD_PROCESS),
+        () => getModulesNamesQueryWrappers(dbType).selectInsertModuleName(data.name, data.belongsToOrganization),
+    ],
+    [
+        equals(INSERT_NEW_MODULE_VERSION_CHILD_PROCESS),
+        () => getModulesVersionsQueryWrappers(dbType).insertNewModuleVersion(data.moduleNameId, data.version),
+    ],
+    [
+        equals(SELECT_MODULE_BY_NAME_ID_AND_VERSION_CHILD_PROCESS),
+        () => getModulesVersionsQueryWrappers(dbType).selectModuleByNameIdAndVersion(data.moduleNameId, data.usrVersion),
+    ],
+    [
+        equals(SELECT_INSERT_MODULE_VERSION_CHILD_PROCESS),
+        () => getModulesVersionsQueryWrappers(dbType).selectInsertModuleVersion(data.moduleNameId, data.version),
+    ],
+    [
+        equals(INSERT_NEW_MODULE_LOCATION_CHILD_PROCESS),
+        () => getModulesLocationsQueryWrappers(dbType).insertNewModuleLocation(data.moduleNameId, data.moduleVersionId, data.path),
+    ],
+    [
+        equals(SELECT_LOCATION_BY_PATH_CHILD_PROCESS),
+        () => getModulesLocationsQueryWrappers(dbType).selectLocationByPath(dbConnection, data.usrPath),
+    ],
+    [
+        equals(SELECT_LOCATION_BY_NAME_ID_AND_VERSION_CHILD_PROCESS),
+        () => getModulesLocationsQueryWrappers(dbType).selectLocationByNameIdAndVersion(dbConnection, data.moduleNameId, data.moduleVersionId, data.usrPath),
+    ],
+    [
+        equals(SELECT_INSERT_MODULE_LOCATION_CHILD_PROCESS),
+        () => getModulesLocationsQueryWrappers(dbType).selectInsertModuleLocation(dbConnection, data.moduleNameId, data.moduleVersionId, data.path),
+    ],
+    [
+        equals(INSERT_NEW_MODULE_LOCATION_CONNECTION_CHILD_PROCESS),
+        () => getModulesLocationConnectionsQueryWrappers(dbType).insertNewModuleLocationConnection(dbConnection, data.moduleLocationId, data.moduleParentLocationId, data.usrType),
+    ],
+    [
+        equals(INSERT_NEW_PATH_CHILD_PROCESS),
+        () => getPathsTraversedQueryWrappers(dbType).insertNewPath(dbConnection, data.path),
+    ],
+    [
+        equals(SELECT_TRAVERSED_PATH_BY_PATH_CHILD_PROCESS),
+        () => getPathsTraversedQueryWrappers(dbType).selectTraversedPathByPath(dbConnection, data.usrPath),
+    ],
+    [
+        stubTrue,
+        Promise.reject(`Undefined task type: '${taskType}'`),
+    ],
+])(taskType);
+
+// export
+exports.executeChildProcessTask = executeChildProcessTask;
