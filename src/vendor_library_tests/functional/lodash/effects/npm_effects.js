630eb9df54b1fb954f96e1bcf99da4c8c8cbc718
sergei <sselihov@brightsign.biz>
2019-09-18 14:20:57 +0300

    Add loadNPMPackageCurrentVersionBy() metgod, code refacktoring.

--- src/vendor_library_tests/functional/lodash/effects/npm_effects.js
+++ src/vendor_library_tests/functional/lodash/effects/npm_effects.js
@@ -2,20 +2,25 @@
 
 // external imports
 const {spawn} = require('child_process');
+const {identity} = require('lodash/fp');
 
 // local imports
+const {SEMVER_NPM_VERSION_SANITIZE_REG_EXP} = require('./../constants/semver_regexp_constants');
 
 // effects implementation
-const loadNPMPackageCurrentVersion = (npmPackageName) => {
+const loadNPMPackageCurrentVersionBy = (transformCallback, npmPackageName) => {
     return new Promise((resolve, reject) => {
         let versionString = null;
         const processLink = spawn('npm', ['view', npmPackageName, 'version']);
 
         processLink.stdout.on('data', data => versionString = data);
         processLink.stderr.on('data', reject);
-        processLink.on('close', () => resolve(versionString.toString()));
+        processLink.on('close', () => resolve(transformCallback(versionString.toString().replace(SEMVER_NPM_VERSION_SANITIZE_REG_EXP, ''))))
     });
 };
 
+const loadNPMPackageCurrentVersion = (npmPackageName) => loadNPMPackageCurrentVersionBy(identity, npmPackageName);
+
 // export
+exports.loadNPMPackageCurrentVersionBy = loadNPMPackageCurrentVersionBy;
 exports.loadNPMPackageCurrentVersion = loadNPMPackageCurrentVersion;
