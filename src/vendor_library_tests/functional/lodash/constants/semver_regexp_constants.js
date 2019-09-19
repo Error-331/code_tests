3fd237b0117b5175dd991f0876aa42645e370028
sergei <sselihov@brightsign.biz>
2019-09-19 21:11:14 +0300

    Work in progress.

--- src/vendor_library_tests/functional/lodash/constants/semver_regexp_constants.js
+++ src/vendor_library_tests/functional/lodash/constants/semver_regexp_constants.js
@@ -9,7 +9,7 @@ const SEMVER_NPM_VERSION_SANITIZE_REG_EXP = RegExp('(\\r\\n|\\n|\\r)', 'g');
 
 const SEMVER_SINGLE_DELIMETER_REG_EXP = RegExp('([^\\|])(\\|)([^\\|])', 'g');
 const SEMVER_PRERELEASE_REG_EXP = RegExp('([0-9]|x|X|\\*)(\\ )*(\-)(\\ )*([A-Za-z])', 'g');
-const SEMVER_OPERATOR_TO_VERSION_REG_EXP = RegExp('(\\<|\\>|\\<\\=|\\>\\=|\\=|\\~|\\^)(\\ )*(\\*|[0-9]|x|X)', 'g');
+const SEMVER_OPERATOR_TO_VERSION_REG_EXP = '(\\<|\\>|\\<\\=|\\>\\=|\\=|\\~|\\^)(\\ )*(\\*|[0-9]|x|X)';
 
 
 const SEMVER_OPERATOR_REG_EXP = RegExp('^\\>|\\<|\\=|\\~|\\^|\\*', 'g');
--- src/vendor_library_tests/functional/lodash/helpers/semver_helpers.js
+++ src/vendor_library_tests/functional/lodash/helpers/semver_helpers.js
@@ -6,10 +6,15 @@ const {
     constant,
     identity,
     isNil,
+    isEmpty,
+    isFinite,
+    min,
+    lte,
     gt,
     gte,
     equals,
     cond,
+    complement,
     curry,
     pipe,
     over,
@@ -19,6 +24,8 @@ const {
     reduce,
     times,
     takeWhile,
+    dropWhile,
+    findLastIndex,
     trim,
     split,
     replace,
@@ -27,6 +34,7 @@ const {
     slice,
     nth,
     size,
+    last,
 } = require('lodash/fp');
 
 // local imports
@@ -43,84 +51,214 @@ const {
 } = require('./../constants/semver_regexp_constants');
 
 // helpers implementation
+//TODO: to helpers
+const findLastFiniteValueIndex = findLastIndex(isFinite);
+//TODO: to helpers
+const findLastFiniteNoneZeroValueIndex = findLastIndex(usrValue => isFinite(usrValue) && usrValue !== 0);
+//TODO: to helpers
+const incrementAtIndex = curry((index, usrArray) => {
+    const newArray = usrArray.slice();
+    newArray[index]++;
+
+    return newArray;
+});
+//TODO: to helpers
+const decrementAtIndex = curry((index, usrArray) => {
+    const newArray = usrArray.slice();
+    newArray[index]--;
+
+    return newArray;
+});
+
+const performOperationAtLastPartOfParsedVersion = curry((callback, versionParts) => {
+    return pipe(
+        findLastFiniteNoneZeroValueIndex,
+        cond([
+            [equals(-1), constant(versionParts)],
+            [stubTrue, index => callback(index, versionParts)]
+        ])
+    )(versionParts);
+});
+
+const incrementLastPartOfParsedVersion =
+    performOperationAtLastPartOfParsedVersion(incrementAtIndex);
+
+const decrementLastPartOfParsedVersion =
+    performOperationAtLastPartOfParsedVersion(decrementAtIndex);
+
+const performTildaOperatorOnParsedVersion = (versionParts) => {
+
+};
+
 const semverStringToParts = (prereleaseTagPartDelimiter, semverVersion) => {
     const [semverVersionPart, semverPrereleaseTagPart] = split(prereleaseTagPartDelimiter, semverVersion);
 
     return compact(concat(
-        pipe(split('.'), map(parseInt))(semverVersionPart),
+        pipe(
+            replace(new RegExp('x|X', 'g'), '*'),
+            split('.'),
+            map(
+                cond([
+                    [equals('*'), constant(Infinity)],
+                    [stubTrue, parseInt]
+                ])),
+            )
+        (semverVersionPart),
         semverPrereleaseTagPart
     ));
 };
 
-const prepareSemverVersion = curry((maxSemverVersion, semverVersion) => {
+const normalizeSemverVersion = curry((maxSemverVersion, semverVersion) => {
+    const normalizedSemverVersion = semverStringToParts('$', semverVersion);
+
     if (size(split('$', semverVersion)) >= 2) {
-        return semverStringToParts('$', semverVersion);
+        return normalizedSemverVersion;
     }
 
-    const preparedSemverVersion = pipe(
-        replace(new RegExp('x|X', 'g'), '*'),
-        split('.'),
-        map(versionPart => equals('*', versionPart) ? '9': versionPart),
-
-        /*cond([
-            [pipe(size, gt(3)), semverVersion => concat(semverVersion, times(constant('0'), 3 - size(semverVersion)))],
-            [stubTrue, identity]
-        ]),*/
-    )(semverVersion);
-
-    const preparedMaxSemverVersion = split('.', maxSemverVersion);
+    const normalizedMaxSemverVersion = semverStringToParts('-', maxSemverVersion);
     const indexesToReplace = takeWhile(
         pipe(
             over([
-                pipe(partIndex => nth(partIndex, preparedSemverVersion), parseInt),
-                pipe(partIndex => nth(partIndex, preparedMaxSemverVersion), parseInt),
+                partIndex => nth(partIndex, normalizedSemverVersion),
+                partIndex => nth(partIndex, normalizedMaxSemverVersion),
             ]),
 
             spread(gte),
         ),
         range(
             0,
-            size(preparedSemverVersion)
+            size(normalizedSemverVersion)
         )
     );
 
-    return map(parseInt,
-        concat(
-            slice(0, size(indexesToReplace), preparedMaxSemverVersion),
-            slice(size(indexesToReplace), size(preparedSemverVersion), preparedSemverVersion)
-        )
+    return concat(
+        slice(0, size(indexesToReplace), normalizedMaxSemverVersion),
+        slice(size(indexesToReplace), size(normalizedSemverVersion), normalizedSemverVersion)
     );
 });
 
-const determineMaxVersionFromRange = curry((npmMaximumVersion, currentMaxVersion, versionRange) => {
-    const semverOperator = cond([
+const extractSemverOperator = (singularRangePart) => {
+    return cond([
         [isNil, constant('')],
         [stubTrue, nth(1)]
-    ])(SEMVER_OPERATOR_TO_VERSION_REG_EXP.exec(versionRange));
+    ])(
+        new RegExp(SEMVER_OPERATOR_TO_VERSION_REG_EXP, 'g').exec(singularRangePart)
+    );
+};
 
-    const currentVersionParts = pipe(
+const extractSemverVersionParts = (npmMaximumVersion, singularRangePart) => {
+    return pipe(
         trim,
-        replace(semverOperator, ''),
-        prepareSemverVersion(npmMaximumVersion)
-    )(versionRange);
+        replace(extractSemverOperator(singularRangePart), ''),
+        normalizeSemverVersion(npmMaximumVersion)
+    )(singularRangePart)
+};
 
-   // console.log('****', semverStringToParts('-', npmMaximumVersion));
-
-    const currentMaxVersionParts = semverStringToParts('-', currentMaxVersion);
-
-    const decrementVersion = (semverVersionString) => {
-
-
-        cond([
-            [semverVersionParts => gt(size(semverVersionParts, 3)), semverVersionParts => split('.', semverVersionParts[size(semverVersionParts) - 1])],
-            [stubTrue, semverVersionParts => semverVersionParts[size(semverVersionParts) - 1] += 1]
-        ])(semverStringToParts(semverVersionString))
+const compareTwoParsedSemverVersions = (firstVer, secondVer) => {
+    if (isNil(secondVer) || isEmpty(secondVer)) {
+        return 1;
     }
 
-    cond([
-        [constant('<'), ]
-        [stubTrue, identity]
-    ])(semverOperator);
+    return cond([
+        [pipe(size, equals(0)), constant(0)],
+        [stubTrue, pipe(
+            nth(0),
+            over([
+                indexToCheck => firstVer[indexToCheck],
+                indexToCheck => secondVer[indexToCheck],
+            ]),
+            spread(gt),
+            cond([
+                [equals(true), constant(1)],
+                [equals(false), constant(-1)]
+            ])
+        )],
+    ])(
+        dropWhile(
+            pipe(
+                over([
+                    partIndex => nth(partIndex, firstVer),
+                    partIndex => nth(partIndex, secondVer),
+                ]),
+                spread(equals),
+            ),
+
+            range(
+                0,
+                min([
+                    size(firstVer),
+                    size(secondVer),
+                ])
+            )
+        )
+    );
+};
+
+const conditionIsFulfilledForTwoParsedSemverVersions = (firstVer, secondVer, condition) => {
+    const comparisonResult = compareTwoParsedSemverVersions(firstVer, secondVer);
+
+    return cond([
+        [equals('='),  constant(equals(comparisonResult, 0))],
+        [equals('>'),  constant(equals(comparisonResult, 1))],
+        [equals('<'),  constant(equals(comparisonResult, -1))],
+        [equals('>='), constant(gte(comparisonResult, 0))],
+        [equals('<='), constant(lte(comparisonResult, 0))],
+    ])(condition)
+};
+
+const semverFullRangeToSections = pipe(
+    // trim whitespaces
+    trim,
+    // replace '|' with '||'
+    versionRange => versionRange.replace(
+        SEMVER_SINGLE_DELIMETER_REG_EXP,
+        (match, part1, part2, part3) => [part1, part3].join('||'),
+    ),
+    // replace '-dev.1' with '$dev.1' for latter parsing (prerelease tag)
+    versionRange => versionRange.replace(
+        SEMVER_PRERELEASE_REG_EXP,
+        (match, part1, part2, part3, part4, part5) => [part1, part5].join('$'),
+    ),
+    // remove `spaces` between semver operator and version specifier
+    versionRange => versionRange.replace(
+        new RegExp(SEMVER_OPERATOR_TO_VERSION_REG_EXP, 'g'),
+        (match, part1, part2, part3) => [part1, part3].join(''),
+    ),
+    // split by '||'
+    split('||'),
+    // remove `empty` values
+    compact,
+);
+
+const determineMaxVersionFromRange = curry((npmMaximumVersion, currentMaxVersion, versionRange) => {
+    const semverOperator = extractSemverOperator(versionRange);
+
+    const currentVersionParts = extractSemverVersionParts(npmMaximumVersion, versionRange);
+    const currentMaxVersionParts = extractSemverVersionParts(npmMaximumVersion, currentMaxVersion);
+
+    //console.log('zpp', semverOperator, isEmpty(semverOperator));
+    //console.log('zpp1', currentMaxVersion, currentMaxVersionParts, isEmpty(currentMaxVersionParts));
+
+    console.log('zpp2', incrementLastPartOfParsedVersion([1, Infinity,4]));
+
+    /*const determineMaxVersionForParsedRange = (versionParts, condition) => {
+        cond([
+            [isEmpty, constant(versionParts)],
+            [constant('='),  constant(versionParts)],
+            [constant('>='), constant(versionParts)],
+            [constant('<='), constant(versionParts)],
+            [constant('>'),  constant(incrementLastPartOfParsedVersion(versionParts))],
+            [constant('<'),  constant(decrementLastPartOfParsedVersion(versionParts))],
+            [constant('~'),  constant(performTildaOperatorOnParsedVersion(versionParts))],
+            [constant('^'),  constant(performCaretOperatorOnParsedVersion(versionParts))]
+        ])(condition)
+    };*/
+
+    //compareTwoParsedSemverVersions(currentVersionParts, currentMaxVersionParts);
+
+    //console.log('compare', conditionIsFulfilledForTwoParsedSemverVersions(currentVersionParts, currentMaxVersionParts, semverOperator));
+
+
 });
 
 /*const fSpace = curry((currentMaxVersion, versionsRange) => pipe(
@@ -151,31 +289,12 @@ const determineMaxVersionFromRangePart = curry((npmMaximumVersion, currentMaxVer
 )(versionsRange));
 
 const determineMaxVersionFromRanges = (npmMaximumVersion, currentMaximumRange, versionsRange) => pipe(
-    // trim whitespaces
-    trim,
-    // replace '|' with '||'
-    versionRange => versionRange.replace(
-        SEMVER_SINGLE_DELIMETER_REG_EXP,
-        (match, part1, part2, part3) => [part1, part3].join('||'),
-    ),
-    // replace '-dev.1' with '$dev.1' for latter parsing (prerelease tag)
-    versionRange => versionRange.replace(
-        SEMVER_PRERELEASE_REG_EXP,
-        (match, part1, part2, part3, part4, part5) => [part1, part5].join('$'),
-    ),
-    // remove `spaces` between semver operator and version specifier
-    versionRange => versionRange.replace(
-        SEMVER_OPERATOR_TO_VERSION_REG_EXP,
-        (match, part1, part2, part3) => [part1, part3].join(''),
-    ),
-    // split by '||'
-    split('||'),
-    // remove `empty` values
-    compact,
+    // split range into sections
+    semverFullRangeToSections,
     // determine max version using range part
     reduce(determineMaxVersionFromRangePart(npmMaximumVersion), currentMaximumRange),
 )(versionsRange);
 
 // export
-exports.prepareSemverVersion = prepareSemverVersion;
+exports.normalizeSemverVersion = normalizeSemverVersion;
 exports.determineMaxVersionFromRanges = determineMaxVersionFromRanges;
