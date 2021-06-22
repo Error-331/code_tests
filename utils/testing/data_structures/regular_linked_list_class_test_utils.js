8bb7904ec7395887d2fbbd2d2e686ee5f62072b2
segei <sergei.error.331@gmail.com>
2021-06-22 03:10:16 +0300

    Add regular linked list test utils.

--- utils/testing/data_structures/regular_linked_list_class_test_utils.js
+++ utils/testing/data_structures/regular_linked_list_class_test_utils.js
@@ -10,9 +10,52 @@ function checkRegularLinkedListEmpty(linkedList) {
 function checkRegularLinkedListNotEmpty(linkedList, size, head, lastChild) {
     assert.strictEqual(linkedList.size, size);
     assert.strictEqual(linkedList.isEmpty, false);
+
     assert.deepStrictEqual(linkedList.head.element, head);
     assert.deepStrictEqual(linkedList.lastChild.element, lastChild);
 }
 
+function checkRegularLinkedListItemAt(linkedList, itemIndex, testItem) {
+    const linkedListItem = linkedList.getElementAt(itemIndex);
+    assert.deepStrictEqual(testItem, linkedListItem.element);
+}
+
+function checkRegularLinkedListIndexOf(linkedList, itemIndex, testItemSearch, testItemStored) {
+    checkRegularLinkedListItemAt(linkedList, itemIndex, testItemStored);
+
+    const linkedListItemIndex = linkedList.indexOf(testItemSearch);
+
+    assert.strictEqual(linkedListItemIndex, itemIndex);
+    checkRegularLinkedListItemAt(linkedList, linkedListItemIndex, testItemStored);
+}
+
+function checkRegularLinkedListItems(linkedList, testItems = []) {
+    const testItemsLength = testItems.length;
+
+    for (let itemsCnt = 0; itemsCnt < testItemsLength; itemsCnt++) {
+        const testItem = testItems[itemsCnt];
+        const linkedListItem = linkedList.getElementAt(itemsCnt).element;
+
+
+        assert.deepStrictEqual(testItem, linkedListItem);
+    }
+}
+
+function checkRegularLinkedListIterator(linkedList, testItems = []) {
+    const iteratedLinkedListItems = [];
+
+    for (const node of linkedList) {
+        iteratedLinkedListItems.push(node.element);
+    }
+
+    assert.deepStrictEqual(testItems, iteratedLinkedListItems);
+}
+
 module.exports.checkRegularLinkedListEmpty = checkRegularLinkedListEmpty;
 module.exports.checkRegularLinkedListNotEmpty = checkRegularLinkedListNotEmpty;
+
+module.exports.checkRegularLinkedListItemAt = checkRegularLinkedListItemAt;
+module.exports.checkRegularLinkedListIndexOf = checkRegularLinkedListIndexOf;
+
+module.exports.checkRegularLinkedListItems = checkRegularLinkedListItems;
+module.exports.checkRegularLinkedListIterator = checkRegularLinkedListIterator;
