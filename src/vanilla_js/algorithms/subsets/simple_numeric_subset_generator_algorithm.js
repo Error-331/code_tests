d3067bc9995ee96e8b36f79da59df1e64df67eef
segei <sergei.error.331@gmail.com>
2021-07-14 23:25:42 +0300

    Add simple subset generator algorithm (naive solution 1).

---
+++ src/vanilla_js/algorithms/subsets/simple_numeric_subset_generator_algorithm.js
@@ -0,0 +1,36 @@
+function simpleNumericSubsetGeneratorAlgorithmNaive1(numberOfElements, currentNumber = 1, subset = [], subsets = []) {
+    if (currentNumber === numberOfElements + 1) {
+        // обработать подмножество
+        if (subset.length === 0) {
+            subset.push(0);
+        }
+
+        subsets.push(subset.slice());
+
+    } else {
+        subset.push(currentNumber);
+        simpleNumericSubsetGeneratorAlgorithmNaive1(numberOfElements, currentNumber + 1, subset.slice(), subsets);
+
+        subset.pop();
+        simpleNumericSubsetGeneratorAlgorithmNaive1(numberOfElements, currentNumber + 1, subset.slice(), subsets);
+    }
+
+    return subsets;
+}
+
+console.log('Simple numeric subset generator algorithm');
+console.log('=========================================');
+
+console.log('');
+
+let generatedSubset = simpleNumericSubsetGeneratorAlgorithmNaive1(3);
+console.log('Generated subset (number of elements - 3, naive implementation 1): ');
+console.log('');
+console.log(generatedSubset);
+
+console.log('');
+
+generatedSubset = simpleNumericSubsetGeneratorAlgorithmNaive1(6);
+console.log('Generated subset (number of elements - 6, naive implementation 1): ');
+console.log('');
+console.log(generatedSubset);
