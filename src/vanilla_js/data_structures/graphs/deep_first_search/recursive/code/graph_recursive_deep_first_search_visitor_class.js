aadcdd6b6e99054eade9ee316f5d60ef24f406f3
serge <sergei.error.331@gmail.com>
2022-01-11 13:38:34 +0200

    Minor fixes.

--- src/vanilla_js/data_structures/graphs/deep_first_search/recursive/code/graph_recursive_deep_first_search_visitor_class.js
+++ src/vanilla_js/data_structures/graphs/deep_first_search/recursive/code/graph_recursive_deep_first_search_visitor_class.js
@@ -4,19 +4,19 @@
 
 // implementation
 class GraphRecursiveDeepFirstSearchVisitorClass {
-    depthFirstSearch(graph, vertexRowId = null, visitedVertexesIds = []) {
+    depthFirstSearch(adjacencyMatrix, vertexRowId = null, visitedVertexesIds = []) {
         let preparedVertexRowId = vertexRowId;
 
         if (preparedVertexRowId === undefined || preparedVertexRowId === null) {
-            const vertexRowIds = graph.rowsIdsIterator;
+            const vertexRowIds = adjacencyMatrix.rowsIdsIterator;
             preparedVertexRowId = vertexRowIds.next().value;
             visitedVertexesIds.push(preparedVertexRowId);
         }
 
-        for (const { element: { id: vertexId } } of graph.getRowById(preparedVertexRowId)) {
+        for (const { element: { id: vertexId } } of adjacencyMatrix.getRowById(preparedVertexRowId)) {
             if (!visitedVertexesIds.includes(vertexId)) {
                 visitedVertexesIds.push(vertexId);
-                this.depthFirstSearch(graph, vertexId, visitedVertexesIds);
+                this.depthFirstSearch(adjacencyMatrix, vertexId, visitedVertexesIds);
             }
         }
 
--- src/vanilla_js/data_structures/graphs/deep_first_search/stack/code/graph_stack_deep_first_search_visitor_class.js
+++ src/vanilla_js/data_structures/graphs/deep_first_search/stack/code/graph_stack_deep_first_search_visitor_class.js
@@ -5,22 +5,20 @@ const RegularStackClass = require('./../../../../stack/regular/code/regular_stac
 
 // implementation
 class GraphStackDeepFirstSearchVisitorClass {
-
-
-    depthFirstSearch(graph, vertexRowId = null) {
+    depthFirstSearch(adjacencyMatrix, vertexRowId = null) {
         const vertexPath = [];
         const valuesInStack = new Set();
         let preparedVertexRowId = vertexRowId;
 
         if (preparedVertexRowId === undefined || preparedVertexRowId === null) {
-            const vertexRowIds = graph.rowsIdsIterator;
+            const vertexRowIds = adjacencyMatrix.rowsIdsIterator;
             preparedVertexRowId = vertexRowIds.next().value;
         }
 
         const visitedVertexesFlags = [];
         const vertexStack = new RegularStackClass();
 
-        visitedVertexesFlags.fill(false, 0, graph.rowsCount);
+        visitedVertexesFlags.fill(false, 0, adjacencyMatrix.rowsCount);
         vertexStack.push(preparedVertexRowId);
 
         while (!vertexStack.isEmpty) {
@@ -28,7 +26,7 @@ class GraphStackDeepFirstSearchVisitorClass {
             visitedVertexesFlags[currentVertexId] = true;
             vertexPath.push(currentVertexId);
 
-            for (const { element: { id: vertexId } } of graph.getRowById(currentVertexId)) {
+            for (const { element: { id: vertexId } } of adjacencyMatrix.getRowById(currentVertexId)) {
                 if (!visitedVertexesFlags[vertexId] && !valuesInStack.has(vertexId)) {
                     vertexStack.push(vertexId);
                     valuesInStack.add(vertexId);
