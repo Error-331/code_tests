1b218fbddf10e0e20604873441205eca9ea49650
Error-331 <red331@mail.ru>
2022-07-10 04:06:19 +0300

    Add basics example for Crypto module.

---
+++ src/node_js/fundamentals/crypto/basics_hashing_out.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const crypto = require('crypto');
+const fs = require('fs');
+
+const key = crypto.randomBytes(16).toString('hex');
+const algorithm = 'aes256';
+
+fs.writeFileSync(`${__dirname}/key.json`, JSON.stringify({ key, algorithm }));
