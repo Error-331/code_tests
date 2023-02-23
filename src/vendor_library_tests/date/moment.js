52b2de6e3da412f17c82298021c99431b75e0567
Sergei <sselihov@brightsign.biz>
2023-02-23 11:52:54 +0200

    Add more moment examples.

--- src/vendor_library_tests/date/moment.js
+++ src/vendor_library_tests/date/moment.js
@@ -7,6 +7,16 @@ export default async () => {
     console.log('======================');
 
     console.log('');
+    console.log('Formatting examples:');
+    console.log('');
+
+    console.log('1/7/2023 is formatted as', moment('1/7/2023').format('YYYY-MM-DD, h:mm:ss a'));
+    console.log('2/4/2023 is formatted as', moment('2/4/2023').format('YYYY-MM-DD, h:mm:ss a'));
+    console.log('1/7/2022 is formatted as', moment('1/7/2022').format('MM/DD/YYYY h:mm:ss a zz'));
+
+    console.log('');
+
+    console.log('');
     console.log('isSame examples:');
     console.log('');
 
