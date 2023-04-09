ca49848d609fdeb2c8ba214837cc90ac9e7fe840
Sergei <sselihov@brightsign.biz>
2023-04-09 22:12:31 +0300

    Major refactor.

--- src/vendor_library_tests/ui/extjs/window_progressbar.js
+++ src/vendor_library_tests/ui/extjs/window_progressbar.js
@@ -2,6 +2,25 @@ let progressBarExt = null;
 let progressBarContainerExt = null;
 let progressBarWindowExt = null;
 
+const isArray = (input) => {
+    return (
+        input instanceof Array ||
+        Object.prototype.toString.call(input) === '[object Array]'
+    );
+};
+
+const isString = (input) => {
+    return typeof input === 'string';
+}
+
+const isNil = (value) => {
+    if (value === undefined || value === null) {
+        return true;
+    }
+
+    return false;
+};
+
 const isNullOrEmpty = (value) => {
     if (isNil(value)) {
         return true;
@@ -101,3 +120,75 @@ function showProgressDialogBoxExt() {
 function hideProgressDialogBoxExt() {
     getProgressDialogBoxExt().hide();
 }
+
+function updateMapReduceProgressDialogBoxExt(stage) {
+    let text = 'Pending...';
+    let value = 0;
+
+    switch(stage) {
+        case 'BUSY': {
+            text = 'Waiting in the queue...';
+            value = 0.1;
+
+            break;
+        }
+
+        case 'GET_INPUT': {
+            text = 'Gathering input data...';
+            value = 0.2;
+
+            break;
+        }
+
+        case 'MAP': {
+            text = 'Processing data...';
+            value = 0.4;
+
+            break;
+        }
+
+        case 'SHUFFLE': {
+            text = 'Shuffling data...';
+            value = 0.6;
+
+            break;
+        }
+
+        case 'REDUCE': {
+            text = 'Combining result data...';
+            value = 0.8;
+
+            break;
+        }
+
+        case 'SUMMARIZE': {
+            text = 'Writing CSV data to a file...';
+            value = 1;
+
+            break;
+        }
+
+        default: {
+            text = 'Pending...';
+            value = 0;
+        }
+    }
+
+    updateProgressBarDialogBoxExt({text, value});
+}
+
+showProgressDialogBoxExt();
+
+const states = ['BUSY', 'GET_INPUT', 'MAP', 'SHUFFLE', 'REDUCE', 'SUMMARIZE']
+let cnt = 0;
+
+let intervalId = setInterval(() => {
+    cnt++;
+
+    updateMapReduceProgressDialogBoxExt(states[cnt]);
+
+    if (cnt >= states.length) {
+        clearInterval(intervalId);
+        hideProgressDialogBoxExt()
+    }
+}, 2000);
